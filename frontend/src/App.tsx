907592193d8703ad0e326ea4e7e76fe32a69a9b9
Moises Weber <moisesweber01@gmail.com>
2021-11-02 13:44:40 -0300

    Static Start

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,34 @@
 import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
+import BarChart from "components/BarChart";
 
 function App() {
     return (
-      <div> 
-         <h1>Olá mundo!</h1>
-         <NavBar />
-     </div>  
+        <>
+       < NavBar />
+        <div className="container">
+            <h1 className="text-primary py-3">Dashboard de vendas</h1>
+
+
+            <div className="row px-3">
+                <div className="col-sm-6">
+                    <h5 className="text-center text-secundary">taxa de sucesso(%)</h5>
+                    <BarChart />
+                </div>
+                <div className="col-sm-6">
+                    <h5 className="text-center text-secundary">todas vendas</h5>
+                    <BarChart />
+                </div>
+            </div>
+
+            <div className="py-3">
+                <h2 className="text-primary">Todas vendas</h2>
+            </div>
+            <DataTable />
+        </div>
+        <Footer />
+        </>
     );
 }
 
